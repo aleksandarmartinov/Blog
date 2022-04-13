938fb9f8a1cda95fe0a4caaa5033d9011a7183ee
Vladimir <vrakita@yahoo.com>
2022-04-13 12:44:03 +0200

    Added app layout

--- src/Views/index.blade.php
+++ src/Views/index.blade.php
@@ -1,49 +1,51 @@
-<?php require_once 'partials/top.php'; ?>
+@extends('layouts/app')
 
-<nav class="navbar navbar-expand navbar-light bg-light">
-    <a href="index.php" class="navbar-brand">Blogger</a>
-    <ul class="navbar-nav ml-auto">
-        <li>
-            @if(isset($_SESSION['logged_user']))
-                <li><a href="add_post_view.php" class="nav-link">Dodaj Post</a></li>
-                <li><a href="logout.php" class="nav-link">Logout</a></li>
-                <li><a href="single_user.php" class="btn btn-warning">{{ $_SESSION['logged_user']->name }}</a></li>
-            @else
-                <li><a href="login_view.php" class="nav-link">Login</a></li>
-                <li><a href="/register" class="nav-link">Register</a></li>
-            @endif
-        </li>
-    </ul>
-</nav>
+@section('content')
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <a href="index.php" class="navbar-brand">Blogger</a>
+        <ul class="navbar-nav ml-auto">
+            <li>
+                @if(isset($_SESSION['logged_user']))
+                    <li><a href="add_post_view.php" class="nav-link">Dodaj Post</a></li>
+                    <li><a href="logout.php" class="nav-link">Logout</a></li>
+                    <li><a href="single_user.php" class="btn btn-warning">{{ $_SESSION['logged_user']->name }}</a></li>
+                @else
+                    <li><a href="login_view.php" class="nav-link">Login</a></li>
+                    <li><a href="/register" class="nav-link">Register</a></li>
+                @endif
+            </li>
+        </ul>
+    </nav>
 
-<div class="jumbotron text-center">
-    <h4>Posts</h4>
-</div>
+    <div class="jumbotron text-center">
+        <h4>Posts</h4>
+    </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-8 offset-2">
-            @foreach($posts as $post)
-            {{ $user->getUserWithId($post->user_id)->name }}
-                <div class="card mb-3">
-                <div class="card-header">
-                <h3>{{ $post->title }}</h3>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 offset-2">
+                @foreach($posts as $post)
+                {{ $user->getUserWithId($post->user_id)->name }}
+                    <div class="card mb-3">
+                    <div class="card-header">
+                    <h3>{{ $post->title }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <p>{{ $post->description }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-info btn-sm float-right">
+                            {{ date_format(date_create($post->created_at),"Y-m-d") }}
+                        </button>
+                        <button class="btn btn-warning btn-sm float-left">
+                            {{ $user->getUserWithId($post->user_id)->name }}
+                        </button>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <p>{{ $post->description }}</p>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-info btn-sm float-right">
-                        {{ date_format(date_create($post->created_at),"Y-m-d") }}
-                    </button>
-                    <button class="btn btn-warning btn-sm float-left">
-                        {{ $user->getUserWithId($post->user_id)->name }}
-                    </button>
-                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
-</div>
-<?php require_once 'partials/bottom.php'; ?>
+@endsection
+
 
